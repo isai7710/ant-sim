b5f737504789e0c276c6f65d3854ccbfbe85e080
Isai Sanzy <isai10sanchez@gmail.com>
2024-12-19 16:08:37 -0700

    adding comments to explain code

--- src/Ant.cpp
+++ src/Ant.cpp
@@ -10,7 +10,10 @@ Ant::Ant(unsigned int windowWidth, unsigned int windowHeight)
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<float> dis(-1.0f, 1.0f);
+
   desiredDirection = sf::Vector2f(dis(gen), dis(gen));
+
+  // normalize direction
   float length = std::sqrt(desiredDirection.x * desiredDirection.x +
                            desiredDirection.y * desiredDirection.y);
   desiredDirection /= length;
@@ -40,20 +43,27 @@ void Ant::randomMove(float deltaTime) {
   static std::mt19937 gen(rd());
   static std::uniform_real_distribution<float> dis(-1.0f, 1.0f);
 
-  float randomDirX = dis(gen);
-  float randomDirY = dis(gen);
-
+  // add small random changes in x and y (randomDir) to the desiredDirection
+  // influenced by the wanderStrength
   sf::Vector2f randomDir(dis(gen), dis(gen));
-  desiredDirection +=
-      sf::Vector2f(randomDir.x * wanderStrength, randomDir.y * wanderStrength);
-  // normalize
+  sf::Vector2f wanderInfluence(randomDir.x * wanderStrength,
+                               randomDir.y * wanderStrength);
+  desiredDirection += wanderInfluence;
+
+  // normalize the desired direction (keep it's direction while making its
+  // magnitude equal to 1) we do this because the desiredDirection will be used
+  // ONLY to change the direction of the ant's velocity vector and not its
+  // magnitude think of it light a compass needle, we only care which way it
+  // points (desiredDirection) and not how long the needle is (magnitude)
   float length = std::sqrt(desiredDirection.x * desiredDirection.x +
                            desiredDirection.y * desiredDirection.y);
   desiredDirection /= length;
 
   sf::Vector2f desiredVelocity = desiredDirection * maxSpeed;
-  sf::Vector2f desiredSteeringForce = desiredVelocity - velocity;
-  sf::Vector2f acceleration = clampVector(desiredSteeringForce, steerStrength);
+
+  sf::Vector2f velocityAdjustment =
+      (desiredVelocity - velocity) * steerStrength;
+  sf::Vector2f acceleration = clampVector(velocityAdjustment, steerStrength);
 
   velocity = clampVector(velocity + acceleration * deltaTime, maxSpeed);
   position += velocity * deltaTime;
@@ -89,9 +99,9 @@ void Ant::moveHome(float deltaTime) {
 */
 
 sf::Vector2f Ant::clampVector(const sf::Vector2f &v, float maxValue) {
-  float length = std::sqrt(v.x * v.x + v.y * v.y);
-  if (length > maxValue) {
-    return v * (maxValue / length);
+  float magnitude = std::sqrt(v.x * v.x + v.y * v.y);
+  if (magnitude > maxValue) {
+    return v * (maxValue / magnitude);
   }
   return v;
 }
