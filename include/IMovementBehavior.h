61322b990ae2dbadc6422acac5bd2715f92fa691
Isai Sanzy <isai10sanchez@gmail.com>
2025-03-10 12:53:38 -0600

    new pure virtual function written so that derived classes MUST implement it, derived classes (movement behaviors) will return a pheromonet type based on the movement behavior they define

--- include/IMovementBehavior.h
+++ include/IMovementBehavior.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Pheromone.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Vector2.hpp>
 
@@ -8,4 +9,5 @@ public:
   virtual sf::Vector2f calculateSteering(const sf::Vector2f &currentPos,
                                          const sf::Vector2f &currentVel,
                                          float deltaTime) = 0;
+  virtual PheromoneType getPheromoneType() const = 0;
 };
